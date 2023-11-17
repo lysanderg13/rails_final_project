adc4b403c0c20ecfa47f5f900c9d1e753d9bdf2d
lysanderg13 <lysanderg13@gmail.com>
2023-11-17 16:46:20 -0600

    Created Models and Seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,49 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require "csv"
+
+ActiveRecord::Base.connection.execute("DELETE FROM sqlite_sequence WHERE name='products';")
+ActiveRecord::Base.connection.execute("DELETE FROM sqlite_sequence WHERE name='categories';")
+
+# open the categories.csv
+file_one = Rails.root.join("db/Categories.csv")
+
+puts "Loading Categories from the CSV file: #{file_one}"
+
+csv_data_one = File.read(file_one)
+
+categories_data = CSV.parse(csv_data_one, headers: true, encoding: "utf-8")
+
+# Loop to add crews
+categories_data.each do |c|
+  categories = Category.create(
+    category_name: c["category_name"]
+  )
+  puts categories.category_name
+end
+puts "Created #{Category.count} Categories."
+
+# open the crews.csv
+file_two = Rails.root.join("db/Product.csv")
+
+puts "Loading Products from the CSV file: #{file_two}"
+
+csv_data_two = File.read(file_two)
+
+products_data = CSV.parse(csv_data_two, headers: true, encoding: "utf-8")
+
+# Loop to add products
+products_data.each do |c|
+  products = Product.create(
+    name:           c["name"],
+    description:    c["description"],
+    price:          c["price"],
+    stock_quantity: c["stock_quantity"],
+    image:          c["image"],
+    category_id:    c["category_id"]
+  )
+  puts products.name
+  puts products.description
+  puts products.price
+  puts products.stock_quantity
+  puts products.category_id
+end
+puts "Created #{Product.count} Products."
