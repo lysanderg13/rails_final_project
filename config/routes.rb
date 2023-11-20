4f9a875529a7a21d54e4b3e95f36c8313afa12cb
lysanderg13 <lysanderg13@gmail.com>
2023-11-19 18:54:53 -0600

    Adding About Controller/View

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  get "about", to: "about#index"
+
+  root to: "home#index"
+
   resources :products, only: %i[index show]
+
   devise_for :admin_users, ActiveAdmin::Devise.config
+
   ActiveAdmin.routes(self)
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
